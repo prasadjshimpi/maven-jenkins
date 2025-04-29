39a61195da56127186fe907d3230522c7201a00c
prasad.s <prasad.s@loginextsolutions.com>
2025-04-29 14:48:59 +0530

    browser parameter added

--- src/test/java/mavenforjenkins/UITest.java
+++ src/test/java/mavenforjenkins/UITest.java
@@ -2,28 +2,53 @@ package mavenforjenkins;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UITest {
 
+	@Parameters("Browser")
 	@Test
-	public void sampleUITest() {
+	public void sampleUITest(String browserName) {
 
-		WebDriverManager.chromedriver().setup();
-		ChromeOptions options = new ChromeOptions();
+		System.out.println("Parameter value is : " + browserName);
+		WebDriver driver = null;
+
+		switch (browserName.toLowerCase()) {
+		case "chrome":
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			break;
+		case "firefox":
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			break;
+		case "safari":
+			WebDriverManager.safaridriver().setup();
+			driver = new SafariDriver();
+			break;
+		case "edge":
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+			break;
+		}
+
+//		ChromeOptions options = new ChromeOptions();
 //		options.addArguments("--headless");
-		WebDriver driver = new ChromeDriver(options);
+//		WebDriver driver = new ChromeDriver(options);
 
 		driver.get("https://demoqa.com/");
 
 		Assert.assertEquals(driver.getTitle(), "DEMOQA", "Title does not matched !");
-		
+
 		System.out.println("Test Complete");
-		
+
 		driver.quit();
 
 	}
